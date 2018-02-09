021419a5af6889d22d6a7dc11b02b5680c82f7f2
sullivane <ewan.sullivan19@alumni.loyola.ca>
2018-02-09 09:12:39 -0500

    Add files via upload

---
+++ Schedule.js
@@ -0,0 +1,50 @@
+/* JavaScript Schedule */
+
+var jamm = getDocumentById("schedulejam").value;
+
+
+/*
+var jams = document.getElementsByClassName("m");
+*/
+
+
+switch (jamm) {
+	case 'music':
+			
+			break;
+	case 'math':
+		
+			break;
+	case 'science':
+		
+			break;
+	case 'religion':
+		
+			break;
+	case 'history':
+		console.log('History')
+			break;
+	case 'french':
+		console.log('French')
+			break;
+	case 'computer programming':
+		console.log('Computer Programming')
+			break;
+	case 'english':
+		console.log('English')
+			break;
+	case 'speech':
+		console.log('Speech')
+			break;
+	case 'homeroom':
+		console.log('Homeroom')
+			break;
+	case 'gym':
+		console.log('Gym')
+			break;
+	case 'level assembly':
+		console.log('Level Assembly')
+			break;
+			
+}
+	
