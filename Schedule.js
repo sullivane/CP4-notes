b86e37a8465a19b8c07eb6ef372c3ccb3fa668c7
sullivane <ewan.sullivan19@alumni.loyola.ca>
2018-02-13 19:09:51 -0500

    Update Schedule.js

--- Schedule.js
+++ Schedule.js
@@ -1,50 +1,96 @@
 /* JavaScript Schedule */
 
-var jamm = getDocumentById("schedulejam").value;
-
-
-/*
-var jams = document.getElementsByClassName("m");
-*/
-
-
-switch (jamm) {
-	case 'music':
-			
-			break;
-	case 'math':
-		
-			break;
-	case 'science':
-		
-			break;
-	case 'religion':
-		
-			break;
-	case 'history':
-		console.log('History')
-			break;
-	case 'french':
-		console.log('French')
-			break;
-	case 'computer programming':
-		console.log('Computer Programming')
-			break;
-	case 'english':
-		console.log('English')
-			break;
-	case 'speech':
-		console.log('Speech')
-			break;
-	case 'homeroom':
-		console.log('Homeroom')
-			break;
-	case 'gym':
-		console.log('Gym')
-			break;
-	case 'level assembly':
-		console.log('Level Assembly')
-			break;
-			
-}
+function searcher() {
 	
+	var jamm = document.getElementById('schedulejam').value;
+
+	switch (jamm) {
+		case 'music':
+			var jams = document.getElementsByClassName("m");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}
+				break;
+		case 'math':
+			var jams = document.getElementsByClassName("ma");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}
+				break;
+		
+		case 'science':
+			var jams = document.getElementsByClassName("s");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}
+				break;
+		case 'religion':
+			var jams = document.getElementsByClassName("r");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}	
+				break;	
+		case 'history':
+	var jams = document.getElementsByClassName("h");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}	
+				break;	
+		case 'french':
+			var jams = document.getElementsByClassName("f");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}	
+				break;	
+		case 'computer programming':
+			var jams = document.getElementsByClassName("cp");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}	
+				break;	
+		case 'english':
+			var jams = document.getElementsByClassName("e");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}
+				break;
+		case 'speech':
+			var jams = document.getElementsByClassName("sp");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}
+				break;
+		case 'homeroom':
+			var jams = document.getElementsByClassName("ho");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}
+				break;
+		case 'gym':
+			var jams = document.getElementsByClassName("g");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}
+				break;
+		case 'level assembly':
+			var jams = document.getElementsByClassName("la");
+			
+			for (i = 0; i < jams.length; i++) {
+				jams[i].style.backgroundColor = "cyan";
+			}
+				break;
+				
+	}
+	return;
+}
